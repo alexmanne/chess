4f410212b662deac07d7298587daab9868554e6a
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-02-18 15:06:58 -0700

    Start UserDao interface

---
+++ server/src/main/java/UserDao.java
@@ -0,0 +1,12 @@
+public interface UserDao {
+
+    void createUser(UserData user) throws DataAccessException; {
+        return;
+    }
+
+    UserData getUser(String username) throws DataAccessException; {
+        return;
+    }
+}
+
+
