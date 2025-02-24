54f0aaa436f720de94db591f95b9fac1712f5c9e
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-02-24 14:05:18 -0700

    Added files

--- server/src/main/java/UserDao.java
+++ server/src/main/java/UserDao.java
@@ -1,12 +1,12 @@
 public interface UserDao {
 
-    void createUser(UserData user) throws DataAccessException; {
-        return;
-    }
-
-    UserData getUser(String username) throws DataAccessException; {
-        return;
-    }
+//    void createUser(UserData user) throws DataAccessException; {
+//        return;
+//    }
+//
+//    UserData getUser(String username) throws DataAccessException; {
+//        return;
+//    }
 }
 
 
