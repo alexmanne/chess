2b9f6cfb911239298348d83ef5f6fa280484605a
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-03-01 09:40:27 -0700

    Added negative tests

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -32,18 +32,28 @@ public class ServiceTests {
     }
 
     @Test
-    public void registerTest() throws DataAccessException {
+    public void goodRegisterTest() throws DataAccessException {
 
         RegisterRequest goodRequest = new RegisterRequest("one", "1234",
                                                           "am@gmail.com");
-        RegisterRequest alreadyTakenRequest = new RegisterRequest("one", "1234",
-                                                                  "am@gmail.com");
-        RegisterRequest badRequest = new RegisterRequest("", "1234",
-                                                         "am@gmail.com");
 
         RegisterResult goodResult = userService.register(goodRequest);
         assertEquals("one", goodResult.username());
         assertNotNull(goodResult.authToken(), "Result did not return auth String");
+
+    }
+
+    @Test
+    public void negativeRegisterTest() throws DataAccessException {
+
+        RegisterRequest goodRequest = new RegisterRequest("one", "1234",
+                "am@gmail.com");
+        RegisterRequest alreadyTakenRequest = new RegisterRequest("one", "1234",
+                "am@gmail.com");
+        RegisterRequest badRequest = new RegisterRequest("", "1234",
+                "am@gmail.com");
+
+        userService.register(goodRequest);
 
         assertThrows(DataAccessException.class, () -> {
             userService.register(alreadyTakenRequest);
@@ -55,16 +65,24 @@ public class ServiceTests {
     }
 
     @Test
-    public void loginTest() throws DataAccessException {
+    public void goodLoginTest() throws DataAccessException {
+
+        LoginRequest goodRequest = new LoginRequest("genemann", "1234");
+        LoginResult goodResult = userService.login(goodRequest);
+
+        assertEquals("genemann", goodResult.username());
+        assertNotNull(goodResult.authToken(), "Result did not return auth String");
+
+    }
+
+    @Test
+    public void negativeLoginTest() throws DataAccessException {
 
         LoginRequest goodRequest = new LoginRequest("genemann", "1234");
         LoginRequest badRequest = new LoginRequest("not a user", "1234");
         LoginRequest wrongPass = new LoginRequest("genemann", "5678");
 
-        LoginResult goodResult = userService.login(goodRequest);
-
-        assertEquals("genemann", goodResult.username());
-        assertNotNull(goodResult.authToken(), "Result did not return auth String");
+        userService.login(goodRequest);
 
         assertThrows(DataAccessException.class, () -> {
             userService.login(badRequest);
@@ -75,7 +93,7 @@ public class ServiceTests {
     }
 
     @Test
-    public void logoutTest() throws DataAccessException {
+    public void goodLogoutTest() throws DataAccessException {
         LoginRequest request1 = new LoginRequest("genemann", "1234");
         LoginResult result1 = userService.login(request1);
 
@@ -85,6 +103,16 @@ public class ServiceTests {
         AuthData auth = authDB.getAuth(authToken1);
         assertNull(auth.authToken());
 
+    }
+
+    @Test
+    public void negativeLogoutTest() throws DataAccessException {
+        LoginRequest request1 = new LoginRequest("genemann", "1234");
+        LoginResult result1 = userService.login(request1);
+
+        String authToken1 = result1.authToken();
+        userService.logout(authToken1);
+
         assertThrows(DataAccessException.class, () -> {
             userService.logout("notAnAuthToken");
         });
@@ -92,17 +120,24 @@ public class ServiceTests {
     }
 
     @Test
-    public void createGameTest() throws DataAccessException {
+    public void goodCreateGameTest() throws DataAccessException {
         CreateRequest goodRequest = new CreateRequest("BestGame", validAuthToken);
         CreateRequest checkGameIDReq = new CreateRequest("2", validAuthToken);
-        CreateRequest emptyRequest = new CreateRequest(null, validAuthToken);
-        CreateRequest invalidAuth = new CreateRequest("gameName", "notAnAuth");
 
         CreateResult goodResult = gameService.createGame(goodRequest);
         CreateResult checkGameIDRes = gameService.createGame(checkGameIDReq);
 
         assertEquals("BestGame", gameDB.getGame(goodResult.gameID()).gameName());
         assertEquals(2, checkGameIDRes.gameID());
+    }
+
+    @Test
+    public void negativeCreateGameTest() throws DataAccessException {
+        CreateRequest goodRequest = new CreateRequest("BestGame", validAuthToken);
+        CreateRequest emptyRequest = new CreateRequest(null, validAuthToken);
+        CreateRequest invalidAuth = new CreateRequest("gameName", "notAnAuth");
+
+        gameService.createGame(goodRequest);
 
         assertThrows(DataAccessException.class, () -> {
             gameService.createGame(emptyRequest);
@@ -114,7 +149,7 @@ public class ServiceTests {
     }
 
     @Test
-    public void listGamesTest() throws DataAccessException {
+    public void goodListGamesTest() throws DataAccessException {
         CreateRequest request1 = new CreateRequest("BestGame", validAuthToken);
         CreateRequest request2 = new CreateRequest("BetterGame", validAuthToken);
         CreateRequest request3 = new CreateRequest("GoodGame", validAuthToken);
@@ -131,14 +166,26 @@ public class ServiceTests {
         // Verify the size
         assertEquals(3, listResult.games().size());
 
-        assertThrows(DataAccessException.class, () -> {
-            gameService.listGames("notAnAuthToken");
-        });
-
     }
 
     @Test
-    public void joinGameTest() throws DataAccessException {
+    public void negativeListGamesTest() throws DataAccessException {
+        CreateRequest request1 = new CreateRequest("BestGame", validAuthToken);
+        CreateRequest request2 = new CreateRequest("BetterGame", validAuthToken);
+        CreateRequest request3 = new CreateRequest("GoodGame", validAuthToken);
+        gameService.createGame(request1);
+        gameService.createGame(request2);
+        gameService.createGame(request3);
+
+        gameService.listGames(validAuthToken);
+
+        assertThrows(DataAccessException.class, () -> {
+            gameService.listGames("notAnAuthToken");
+        });
+    }
+
+    @Test
+    public void goodJoinGameTest() throws DataAccessException {
         CreateRequest request1 = new CreateRequest("BestGame", validAuthToken);
         gameService.createGame(request1);
 
@@ -146,6 +193,15 @@ public class ServiceTests {
         gameService.joinGame(goodRequest);
         GameData game = gameDB.getGame(1);
         assertEquals("genemann", game.whiteUsername());
+    }
+
+    @Test
+    public void negativeJoinGameTest() throws DataAccessException {
+        CreateRequest request1 = new CreateRequest("BestGame", validAuthToken);
+        gameService.createGame(request1);
+
+        JoinRequest goodRequest = new JoinRequest(validAuthToken, "WHITE", 1);
+        gameService.joinGame(goodRequest);
 
         JoinRequest badRequest = new JoinRequest(validAuthToken, "notAColor", 1);
         JoinRequest wrongGameID = new JoinRequest(validAuthToken, "BLACK", 3);
@@ -170,7 +226,7 @@ public class ServiceTests {
     }
 
     @Test
-    public void clearTest() throws DataAccessException {
+    public void goodClearTest() throws DataAccessException {
 
         RegisterRequest goodRequest = new RegisterRequest("one", "1234",
                 "am@gmail.com");
@@ -188,6 +244,18 @@ public class ServiceTests {
         assertEquals("one", goodResult.username(), "Did not remove user 'one");
         assertEquals("two", goodResult1.username(), "Did not remove user 'two");
         assertEquals("three", goodResult2.username(), "Did not remove user 'three");
+
+    }
+
+    @Test
+    public void negativeClearTest() throws DataAccessException {
+
+        RegisterRequest goodRequest = new RegisterRequest("one", "1234",
+                "am@gmail.com");
+
+        userService.register(goodRequest);
+
+        userService.clear();
 
         assertNull(userDB.getUser("one").username());
     }
