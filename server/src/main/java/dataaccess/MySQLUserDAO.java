297b2ca5e34173bef916404690adabd734426ed4
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-03-08 17:02:43 -0700

    Started UserDAO

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -2,7 +2,13 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
 public class MySQLUserDAO implements UserDao {
+
+    public MySQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
 
     @Override
@@ -18,5 +24,30 @@ public class MySQLUserDAO implements UserDao {
     @Override
     public void clear() {
 
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(username),
+            );
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (DataAccessException | SQLException ex) {
+            throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
