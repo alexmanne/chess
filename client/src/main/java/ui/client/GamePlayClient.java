bbf38c3ec73e10342da867d8fc60b98eb771dab4
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-03-25 09:32:52 -0600

    Started draw board

--- client/src/main/java/ui/client/GamePlayClient.java
+++ client/src/main/java/ui/client/GamePlayClient.java
@@ -13,4 +13,14 @@ public class GamePlayClient {
     public String eval(String inputLine) {
         return "evaluated";
     }
+
+    // Creates the string to print a new board to the screen from white perspective
+    public static String drawNewWhiteBoard () {
+        return null;
+    }
+
+    // Creates the string to print a new board to the screen from black perspective
+    public static String drawNewBlackBoard () {
+        return null;
+    }
 }
