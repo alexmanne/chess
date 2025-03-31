d5f443388e4e1157d9426581831b3d70d83df5d1
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-03-31 14:25:30 -0600

    Added files

---
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -0,0 +1,13 @@
+package websocket;
+
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+
+public class WebSocketFacade extends Endpoint {
+
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
